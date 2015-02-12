00079eb65810d14d8920cda38426efff461a65c7
Yigit Guler <yigit@hipo.biz>
2015-02-12 15:56:46 +0200

    Fix circular import error

--- sloop/tasks.py
+++ sloop/tasks.py
@@ -3,14 +3,12 @@ from django.conf import settings
 from celery.task import task
 
 
-concrete_device_model = get_model(*settings.SLOOP_DEVICE_TOKEN_MODEL.split("."))
-
-
 @task()
 def send_push_notification(token_id, message, url, sound, extra, category):
     """
     Sends a push notification message to the specified tokens
     """
+    concrete_device_model = get_model(*settings.SLOOP_DEVICE_TOKEN_MODEL.split("."))
     device = concrete_device_model.objects.get(id=token_id)
     device.send_push_notification(message, url, sound, extra, category)
     return "Message: %s" % message
@@ -20,6 +18,7 @@ def send_silent_push_notification(token_id, extra, content_available):
     """
     Sends a push notification message to the specified tokens
     """
+    concrete_device_model = get_model(*settings.SLOOP_DEVICE_TOKEN_MODEL.split("."))
     device = concrete_device_model.objects.get(id=token_id)
     device.send_silent_push_notification(extra, content_available)
     return "Silent push"
