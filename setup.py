462be4e0260dee7f4813a646758d9bb9cb78a040
Yigit Guler <yigit@hipo.biz>
2015-05-06 17:26:48 +0300

    Failed attempts

--- setup.py
+++ setup.py
@@ -8,14 +8,13 @@ install_requires = [
 setup(
   name = 'sloop',
   packages = ['sloop'], # this must be the same as the name above
-  version = '0.3.6.2',
+  version = '0.3.6.3',
   description = 'Django package of the sloop push notification service',
   author = 'H. Yigit Guler',
   author_email = 'yigit@hipolbs.com',
   url = 'https://github.com/hipo/django-sloop', # use the URL to the github repo
   keywords='django, push',
   include_package_data=True,
-  package_data={'templates':'templates/sloop/push_notification.html'},
   classifiers = [],
   install_requires=install_requires,
 
