b6cda5890518249458d95fa44adccd340945812c
Ali <alicertel@gmail.com>
2019-08-09 21:56:18 +0300

    remove unused codes

--- test_app/devices/admin.py
+++ test_app/devices/admin.py
@@ -1,15 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-
-from django_sloop.admin import SloopAdminMixin
 from test_app.devices.models import Device
 
-
-class CustomUserAdmin(SloopAdminMixin, UserAdmin):
-    actions = ("send_push_notification", )
-
-
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
 admin.site.register(Device)
