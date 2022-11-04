01a434f83079c70d7576a1a90db1c5f87a286ba4
avesda <deniz.altun@hipo.biz>
2022-11-04 16:09:58 +0300

    Use gettext_lazy and only use default_app_config for django version < 3.2

--- django_sloop/models.py
+++ django_sloop/models.py
@@ -5,7 +5,7 @@ from django.contrib.gis.db import models
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
 from django.utils.encoding import smart_text
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.template.defaultfilters import truncatechars
 
 from django_sloop.exceptions import DeviceIsNotActive
